bf0362b91ee990da1ac45295e58dee0040de4395
Dhruv  Bhasin <dhruvbhasin0007@gmail.com>
2024-11-27 15:30:15 +0530

    Authentication added

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 def home(request):
+    #check to see if they are logging
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate( request , username = username , password = password)
+        if user is not None:
+            login(request , user)
+            messages.success(request , "you have been logged in")
+            return redirect ('home')
+        else:
+            messages.success(request , "There was an error please try again...")
+            return redirect('home')
+
     return render (request , 'home.html' , {})
 def login_user( request ):
     pass
